85e77d06b2ac9582a93834edd56e43eb0ad9e476
Aaron William Po <apo2@uwo.ca>
2022-04-23 23:24:06 -0400

    Parsing the url string

--- backend/socket/src/main/java/RequestProcessor.java
+++ backend/socket/src/main/java/RequestProcessor.java
@@ -28,6 +28,54 @@ public class RequestProcessor {
 
   public void run() throws IOException {
     //write your code here
+    String request = (in.readLine());
+    String[] parsedRequestParams = request
+      .split(" ")[1].split("/")[1].split("&");
+
+    HashMap<String, String> calcOperations = new HashMap<String, String>();
+
+    for (String requestParam : parsedRequestParams) {
+      String[] keyValues = requestParam.split("=");
+      String key = (keyValues[0]);
+      String value = keyValues[1];
+
+      calcOperations.put(key, value);
+    }
+
+    float leftOperand, rightOperand;
+
+    leftOperand = Float.parseFloat(calcOperations.get("leftOperand"));
+    rightOperand = Float.parseFloat(calcOperations.get("rightOperand"));
+
+    float answer;
+    switch (calcOperations.get("operation")) {
+      case "+":
+        answer = (rightOperand + leftOperand);
+        break;
+      case "-":
+        answer = (rightOperand - leftOperand);
+        break;
+      case "*":
+        answer = (rightOperand * leftOperand);
+        break;
+      case "/":
+        answer = (rightOperand / leftOperand);
+        break;
+      case "%":
+        answer = (rightOperand % leftOperand);
+        break;
+      default:
+        answer = 0;
+    }
+
+    String expression =
+      calcOperations.get("leftOperand") +
+      " " +
+      calcOperations.get("operation") +
+      " " +
+      calcOperations.get("rightOperand");
+    jsonObject.put("expression", expression);
+    jsonObject.put("result", answer);
 
     // end of your code
     String response = msgToClient + jsonObject.toString();
